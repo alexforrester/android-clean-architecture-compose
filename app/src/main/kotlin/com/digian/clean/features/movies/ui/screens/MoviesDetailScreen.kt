7259999c58554a810a1f75cebb3ccc82f37503c8
alexforrester <aforresterwork@gmail.com>
2023-12-19 17:29:09 +0000

    Adds image loading

--- app/src/main/kotlin/com/digian/clean/features/movies/ui/screens/MoviesDetailScreen.kt
+++ app/src/main/kotlin/com/digian/clean/features/movies/ui/screens/MoviesDetailScreen.kt
@@ -1,8 +1,19 @@
 package com.digian.clean.features.movies.ui.screens
 
 import android.annotation.SuppressLint
+import android.net.Uri
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberImagePainter
+import com.digian.clean.features.movies.domain.entities.GenreEntity
+import com.digian.clean.features.movies.domain.entities.MovieEntity
+import com.digian.clean.features.movies.presentation.IMAGE_URL_AND_PATH
 import com.digian.clean.features.movies.presentation.MovieDetailViewModelCompose
 
 
@@ -14,7 +25,57 @@ fun MovieDetailScreen(
     onNavigateBack: () -> Unit = {}
 ) {
 
+    val movieEntity: State<MovieEntity?> = viewModel.movie.observeAsState()
+    
+    DisplayMovie(movieEntity = movieEntity.value)
+    
+
+
 
 
 }
+@Composable
+fun DisplayMovie(movieEntity: MovieEntity?) {
+
+    movieEntity?.run {
+
+        val posterImage = IMAGE_URL_AND_PATH.plus(movieEntity.imagePath)
+
+        AsyncImage(
+            model = posterImage,
+            contentDescription = null,
+        )
+
+    }
+
+}
+
+
+@Composable
+fun TitleText (modifier: Modifier, text: String) {
+    OutlinedTextField(value = text, onValueChange = {}, modifier = modifier)
+}
+
+
+
+
+fun createGenreText(genreData: List<GenreEntity>): String {
+    val genreNames = genreData.map { genre -> genre.name }
+
+    if (genreData.isEmpty()) {
+        return ""
+    }
+
+    var genresText = "GENRES: "
+
+    genreNames.forEach { genre ->
+        genresText += genre.plus(", ")
+    }
+
+    return genresText.trimEnd().substringBeforeLast(",")
+}
+
+
+
+
 
